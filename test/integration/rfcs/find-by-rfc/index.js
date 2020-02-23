9cd413156de1277e9e2903bd95e212f57875c9e7
Manuel de la Torre <manuelmhtr@gmail.com>
2020-02-22 19:28:57 -0600

    Replaces .findValidByRfc in favor of .findByRfc

--- test/integration/rfcs/find-by-rfc/index.js
+++ test/integration/rfcs/find-by-rfc/index.js
@@ -2,7 +2,7 @@ const database = require(`${ROOT_PATH}/lib`);
 
 const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
 
-describe('Database | RFCs store | .findValidByRfc', () => {
+describe('Database | RFCs store | .findByRfc', () => {
   const fixtures = require('./fixtures');
   const [RFC_1] = fixtures.rfcs;
 
@@ -12,30 +12,39 @@ describe('Database | RFCs store | .findValidByRfc', () => {
   });
 
   it('should return null when rfc does not match', async () => {
-    const updatedAfter = new Date(Date.now() - ONE_DAY_IN_MS);
-    const rfc = await database.rfcs.findValidByRfc('NON_EXISTENT_RFC', updatedAfter);
+    const rfc = await database.rfcs.findByRfc('NON_EXISTENT_RFC');
     expect(rfc).to.be.equal(null);
   });
 
-  it('should return RFC obj when rfc matches and updatedAt is after updatedAfter', async () => {
+  it('should return RFC obj when rfc matches', async () => {
+    const rfc = await database.rfcs.findByRfc(RFC_1.rfc);
+
+    expect(rfc).to.be.an('object');
+    expect(rfc.id).to.be.equal(RFC_1.id);
+    expect(rfc.rfc).to.be.equal(RFC_1.rfc);
+  });
+
+  it('should return RFC when rfc matches and updatedAt option is after updatedAfter', async () => {
     const updatedAfter = new Date(Date.now() - ONE_DAY_IN_MS);
-    const rfc = await database.rfcs.findValidByRfc(RFC_1.rfc, updatedAfter);
+    const rfc = await database.rfcs.findByRfc(RFC_1.rfc, { updatedAfter });
+
     expect(rfc).to.be.an('object');
     expect(rfc.id).to.be.equal(RFC_1.id);
     expect(rfc.rfc).to.be.equal(RFC_1.rfc);
     expect(rfc.updatedAt.getTime() > updatedAfter.getTime()).to.be.equal(true);
   });
 
-  it('should return null when rfc matches but updatedAt is before updatedAfter', async () => {
+  it('should return null when rfc matches but updatedAt option is before updatedAfter', async () => {
     const updatedAfter = new Date(Date.now() + ONE_DAY_IN_MS);
-    const rfc = await database.rfcs.findValidByRfc(RFC_1.rfc, updatedAfter);
+    const rfc = await database.rfcs.findByRfc(RFC_1.rfc, { updatedAfter });
+
     expect(rfc).to.be.equal(null);
   });
 
   it('should support fields selection', async () => {
     const fields = ['id', 'rfc'];
-    const updatedAfter = new Date(Date.now() - ONE_DAY_IN_MS);
-    const rfc = await database.rfcs.findValidByRfc(RFC_1.rfc, updatedAfter, {fields});
+    const rfc = await database.rfcs.findByRfc(RFC_1.rfc, {fields});
+
     expect(rfc).to.be.an('object');
     expect(rfc.id).to.be.equal(RFC_1.id);
 
@@ -44,7 +53,7 @@ describe('Database | RFCs store | .findValidByRfc', () => {
   });
 
   it('should throw error when not sending an id', () => {
-    return expect(database.rfcs.findValidByRfc()).to.be.rejectedWith('Undefined binding(s) detected when compiling FIRST. Undefined column(s): [rfc] query: select * from "rfcs" where "rfc" = ? and "updated_at" > ? limit ?');
+    return expect(database.rfcs.findByRfc()).to.be.rejectedWith('Undefined binding(s) detected when compiling FIRST. Undefined column(s): [rfc] query: select * from "rfcs" where "rfc" = ? and "updated_at" >= ? limit ?');
   });
 
   describe('Returning null', () => {
@@ -52,7 +61,7 @@ describe('Database | RFCs store | .findValidByRfc', () => {
 
     NULL_TEST_CASES.forEach(testCase => {
       it(`should return null when no place exist with an id (${testCase})`, async () => {
-        const result = await database.rfcs.findValidByRfc(testCase);
+        const result = await database.rfcs.findByRfc(testCase);
         expect(result).to.be.equal(null);
       });
     });
